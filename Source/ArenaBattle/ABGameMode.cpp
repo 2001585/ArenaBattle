cb9dcd9d115aeb016045fb4c96786ac081be9629
2001585 <hoonso20@kunsan.ac.kr>
2024-09-18 20:19:36 +0900

    #7 GTA Movement Complete

--- Source/ArenaBattle/ABGameMode.cpp
+++ Source/ArenaBattle/ABGameMode.cpp
@@ -1,12 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ABGameMode.h"
-#include "ABPawn.h"
+#include "ABCharacter.h"
 #include "ABPlayerController.h"
 
 AABGameMode::AABGameMode()
 {
-	DefaultPawnClass = AABPawn::StaticClass();
+	DefaultPawnClass = AABCharacter::StaticClass();
 
 	// 새로운 플레이어 컨트롤러의 코드가 생성되면 게임 모드의 생성자에서 PlayerController 속성을 ABPlayerController
 	// 의 클래스 값으로 변경한다.
@@ -21,3 +21,4 @@ void AABGameMode::PostLogin(APlayerController* NewPlayer)
 	Super::PostLogin(NewPlayer);
 	ABLOG(Warning, TEXT("PostLogin End"));
 }
+
